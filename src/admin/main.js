eefaa425401095a400c3aca6ce8dd0d406963aa4
Rey C <reygcalantaol@gmail.com>
2026-02-12 15:45:16 +0800

    Updates

--- src/admin/main.js
+++ src/admin/main.js
@@ -1,30 +1,30 @@
 import "./main.css";
-
-/**
- * If elementor does not exists, exit.
- */
-if (!elementor) {
-    return;
-}
-
-/**
- * Render the file uploader field in the editor.
- */
-elementor.hooks.addFilter('elementor_pro/forms/content_template/field/easy-dragdrop-upload', function (inputField, item, i, settings) { 
-    const itemClasses = _.escape(item.css_classes),
-    required = '',
-    multiple = '',
-    fieldName = 'form_field_';
-
-    if (item.required) {
-        required = 'required';
+(function () {
+    /**
+     * If elementor does not exists, exit.
+     */
+    if (!elementor) {
+        return;
     }
 
-    if (item.allow_multiple_upload) {
-        multiple = ' multiple="multiple"';
-        fieldName += '[]';
-    }
+    /**
+     * Render the file uploader field in the editor.
+     */
+    elementor.hooks.addFilter('elementor_pro/forms/content_template/field/easy-dragdrop-upload', function (inputField, item, i, settings) { 
+        const itemClasses = _.escape(item.css_classes),
+        required = '',
+        multiple = '',
+        fieldName = 'form_field_';
 
-    return '<input size="1"  type="file" class="elementor-file-field elementor-field elementor-size-' + settings.input_size + ' ' + itemClasses + '" name="' + fieldName + '" id="form_field_' + i + '" ' + required + multiple + ' >';
-}, 10, 4);
+        if (item.required) {
+            required = 'required';
+        }
 
+        if (item.allow_multiple_upload) {
+            multiple = ' multiple="multiple"';
+            fieldName += '[]';
+        }
+
+        return '<input size="1"  type="file" class="elementor-file-field elementor-field elementor-size-' + settings.input_size + ' ' + itemClasses + '" name="' + fieldName + '" id="form_field_' + i + '" ' + required + multiple + ' >';
+    }, 10, 4);
+})();
